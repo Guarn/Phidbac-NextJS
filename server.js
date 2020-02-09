a90b4918e4e08f295f49de53552ed6f1fe738ba7
Florian Jung - Fixe <florianjung885@gmail.com>
2020-02-09 09:36:54 +0100

    Ajout Responsive

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const https = require("https");
 const fs = require("fs");
-
+const http2 = require("http2");
+/*
 https
   .createServer(
+    {
+      key: fs.readFileSync("/etc/letsencrypt/live/phidbac.fr/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/phidbac.fr/cert.pem"),
+      ca: fs.readFileSync("/etc/letsencrypt/live/phidbac.fr/chain.pem")
+    },
+    handler
+  )
+  .listen(7000);
+*/
+
+http2
+  .createSecureServer(
     {
       key: fs.readFileSync("/etc/letsencrypt/live/phidbac.fr/privkey.pem"),
       cert: fs.readFileSync("/etc/letsencrypt/live/phidbac.fr/cert.pem"),
