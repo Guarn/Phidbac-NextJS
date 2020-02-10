41efae2dcf77e5a3ae71abaad4bb4b06d1e27fc6
Florian Jung - Fixe <florianjung885@gmail.com>
2020-02-10 06:48:22 +0100

    Service worker

--- next.config.js
+++ next.config.js
@@ -2,32 +2,35 @@
 
 const withCss = require("@zeit/next-css");
 const path = require("path");
+const withOffline = require("next-offline");
 
-module.exports = withCss({
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === "function") {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals)
-      ];
+module.exports = withOffline(
+  withCss({
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style\/css.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === "function") {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === "function" ? [] : origExternals)
+        ];
 
-      config.module.rules.unshift({
-        test: antStyles,
-        use: "null-loader"
-      });
+        config.module.rules.unshift({
+          test: antStyles,
+          use: "null-loader"
+        });
+      }
+      return config;
     }
-    return config;
-  }
-});
+  })
+);
 /*
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: true
