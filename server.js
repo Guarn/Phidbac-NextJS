2c236a75932ff8528d5e9d0c894b43f78a829f01
Florian Jung - Fixe <florianjung885@gmail.com>
2020-02-10 06:56:37 +0100

    Service worker

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ const app = next({
 const express = require("express");
 const handler = app.getRequestHandler();
 
+const { createServer } = require("http");
+const { join } = require("path");
+const { parse } = require("url");
+const next = require("next");
+
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    // handle GET request to /service-worker.js
+    if (pathname === "/service-worker.js") {
+      const filePath = join(__dirname, ".next", pathname);
+
+      app.serveStatic(req, res, filePath);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, () => {
+    console.log(`> Ready on http://localhost:${7000}`);
+  });
+});
+/*
 app.prepare().then(() => {
   const server = express();
   server.all("*", (req, res) => {
@@ -18,7 +44,7 @@ app.prepare().then(() => {
     console.log("> Ready on http://localhost:3000");
   });
 });
-/*
+
 https
   .createServer(
     {
