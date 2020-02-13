21c4ca087326c5e64e6ae8f4b819f45388e538d5
Florian Jung - Fixe <florianjung885@gmail.com>
2020-02-13 11:21:27 +0100

    Changement base ui pour drawer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,7 +33,6 @@ export default class Document extends NextDocument {
     return (
       <Html lang="fr">
         <Head>
-          <link rel="shortcut icon" href="/favicon.jpg" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#e2e0d8" />
           <link
