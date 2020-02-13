264538f8d60fa10f747149b1510e068f464aaa36
Florian Jung - Fixe <florianjung885@gmail.com>
2020-02-13 10:26:07 +0100

    Changement base ui pour drawer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NextApp from "next/app";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 
 const GlobalStyle = createGlobalStyle`
   html,body, #__next {
